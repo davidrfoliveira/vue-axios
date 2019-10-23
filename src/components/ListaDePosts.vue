8ea4491d85504b20cf357a1d6a75c6e96592e71e
David Rodrigues Oliveira <david.oliveira@compline.com.br>
2019-10-23 10:14:17 -0300

    Criação do componente para listagem de posts

---
+++ src/components/ListaDePosts.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>title</th>
+          <th>body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post of lista" :key="post.id">
+          <td scope="row">{{post.id}}</td>
+          <td>{{post.title}}</td>
+          <td>{{post.body}}</td>
+        </tr>
+        
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import posts from '../services/postService'
+
+export default{
+  data(){
+    return{
+      lista : []
+    }
+  },
+  mounted(){
+    posts.listarPosts().then(resposta => {
+     this.lista = resposta.data
+    })
+  }
+}
+
+</script>
+
+<style>
+
+</style>
